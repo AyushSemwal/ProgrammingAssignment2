2e04437eaaaeba5395b8db998e588e1de98ed484
AyushSemwal <ayush.semwal@gmail.com>
2015-06-21 22:28:01 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,40 @@
-## makeCacheMatrix contains 4 functions: set, get, setsolve, getsolve
-## get:returns the matrix x stored in the main function
-## set:changes the matrix stored in the main function
+## makeCacheMatrix contains 4 functions:  set, get, setsolve, getsolve
+## get: returns the matrix x stored in the main function
+## set: changes the matrix stored in the main function
 
-## setsolve and getsolve:store the value of the input in a variable m 
-## into the main function makeCacheMatrix (setsolve) 
-## and return it (getsolve)
+##  setsolve and getsolve:store the value of the input in a variable m 
+##  into the main function makeCacheMatrix(setsolve) 
+##  and return it  (getsolve)
 
-makeCacheMatrix <- function(x = matrix()) {
-        m <-  NULL
-        set <-  function(y) {
-                x <<-  y
-                m <<-  NULL
-         }
-        get <-  function() x
-        setsolve <-  function(solve) m <<- solve
-        getsolve <-  function() m
-        list(set =  set, get =  get,
-             setsolve =  setsolve,
-             getsolve =  getsolve)
-}
-
-
-## verify the value m, stored previously with getsolve, exists
-## and is not NULL
-## If it exists in memory, returns a message and the value m
-
-cacheSolve <-  function(x, ...) {
-        m <-  x$getsolve()
-        if(!is.null(m))  {
-                message("getting cached data")
-                return(m )
+makeCacheMatrix <-function(x = matrix()) {
+        m <-NULL
+        set <-function(y) {
+                x <<-y
+                m <<-NULL
         }
-        data <-  x$get()
-        m <-  solve(data, ...)
-        x$setsolve(m )
-        m }
+        get <-function() x
+        setsolve <-function(solve) m <<- solve
+        getsolve <-function() m
+        list(set=set, get=get,
+             setsolve=setsolve,
+             getsolve=getsolve)
+   
+   }
+
+
+##   verify the value m, stored previously with getsolve, exists
+##   and is not NULL
+##   If it exists in memory, returns a message and the value m
+
+cacheSolve <-function(x, ...) {
+        m <-x$getsolve()
+        if(!is.null(m)){
+                message("getting cached  data")
+                return(m)
+          }
+        data <-x$get()
+        m <-solve(data, ...)
+        x$setsolve(m)
+        m 
+        
+}
